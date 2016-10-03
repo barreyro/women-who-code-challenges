0df410fe41293cbbe02cb559d5c2a366027fd4c4
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2016-10-03 10:15:51 -0400

    search through LL

--- ctci/linkedLists.js
+++ ctci/linkedLists.js
@@ -24,3 +24,27 @@ LinkedList.prototype.add = function(value) {
 
   return node;
 };
+
+LinkedList.prototype.searchNodeAt = function(position){
+  var currentNode = this.head,
+    length = this._length,
+    count = 1,
+    message = {failure: 'non-existent node'};
+  
+  if (length === 0 || position < 1 || position > length){
+    throw new Error(message.failure);
+  }
+
+  while (count < position){
+    currentNode = currentNode.next;
+    count++;
+  }
+  return currentNode;
+};
+
+
+
+
+
+
+
