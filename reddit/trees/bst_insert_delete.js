41f71fa60880adb193a877ec8b4ae8e8218bca97
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2016-10-10 19:37:19 -0400

    add insert function

--- reddit/trees/bst_insert_delete.js
+++ reddit/trees/bst_insert_delete.js
@@ -1,34 +1,42 @@
 //implement BST with insert and delete functions
 
+function BST(root) {
+  this.root = root || '';
+}
 
-var BST = function () {
-  this.root       = '';
-  this.leftChild  = '';
-  this.rightChild = '';
-};
+function Node(value, parentNode, leftChild, rightChild){
+  this.value      =  value;
+  this.parentNode =  parentNode || '';
+  this.leftChild  =  leftChild || '';
+  this.rightChild =  rightChild || '';
+}
 
-class Node {
-  constructor(value) {
-    this.value      =  '',
-    this.parentNode =  '',
-    this.leftChild  =  '',
-    this.rightChild =  ''
-  }
-};
+BST.prototype.addNode = function (value, parentNode, leftChild, rightChild){
+  var newNode = new Node(value, parentNode, leftChild, rightChild);
 
-BST.prototype.addNode(value){
-  function checkRoot(value){
-    if (!this.root){ 
-      this.root = value;
-    } 
-  }
+  if (!this.root){ 
+    this.root = newNode;
+    return;
+  } 
 
-  function checkChildren(value){
-    if (this.r){
+  var parent  = this.root;
+  var found   = false;
+
+  while (found === false){
+    if (newNode.value < parent.value){
+      if (!parent.leftChild) {
+        parent.leftChild = newNode;
+        found = true;
+      } else if (parent.leftChild && (newNode.value < parent.leftChild.value)) {
+        parent = parent.leftChild;
+      }
+    } else if (newNode.value > parent.value) {
+      if (!parent.rightChild) {
+        parent.rightChild = newNode;
+        found = true;
+      } else if (parent.rightChild && (newNode.value > parent.leftChild.value)) {
+        parent = parent.leftChild;
+      }
     }
   }
-
-
-    (this.root > value) ? :
-  
-}
+};
