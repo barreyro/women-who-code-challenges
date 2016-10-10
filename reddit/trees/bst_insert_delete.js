99f62d0919ecdab37434504cf1c2664fc27777f2
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2016-10-10 17:37:08 -0400

    reoragnize fodlers

---
+++ reddit/trees/bst_insert_delete.js
@@ -0,0 +1,34 @@
+//implement BST with insert and delete functions
+
+
+var BST = function () {
+  this.root       = '';
+  this.leftChild  = '';
+  this.rightChild = '';
+};
+
+class Node {
+  constructor(value) {
+    this.value      =  '',
+    this.parentNode =  '',
+    this.leftChild  =  '',
+    this.rightChild =  ''
+  }
+};
+
+BST.prototype.addNode(value){
+  function checkRoot(value){
+    if (!this.root){ 
+      this.root = value;
+    } 
+  }
+
+  function checkChildren(value){
+    if (this.r){
+    }
+  }
+
+
+    (this.root > value) ? :
+  
+}
